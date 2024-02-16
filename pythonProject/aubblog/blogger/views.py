a1fd00751da534cc2c14d063924dffe3efe5dbe1
HBchaos <37412021+HBchaos@users.noreply.github.com>
2024-02-16 19:38:41 +0530

    Add files via upload

---
+++ pythonProject/aubblog/blogger/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+
+# Create your views here.
+# blog/views.py
+from django.http import HttpResponseRedirect
+from blogger.forms import CommentForm
+from blogger.models import Post, Comment
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-publication_date")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/index.html", context)
+
+# blog/views.py
+
+# ...
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-publication_date")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "blog/category.html", context)
+
+# blog/views.py
+
+# ...
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                content=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": CommentForm(),
+    }
+
+    return render(request, "blog/details.html", context)
